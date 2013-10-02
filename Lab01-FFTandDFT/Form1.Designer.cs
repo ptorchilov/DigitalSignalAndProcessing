dd0a5d1a7fe0817bf528af92f8ae4f1ee0c5c67b
p.torchilov <p.torchilov@gmail.com>
2013-10-03 00:17:53 +0300

    Initial refactoring, starting read lab condition and write code.

--- Lab01-FFTandDFT/Form1.Designer.cs
+++ Lab01-FFTandDFT/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(624, 442);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1Load);
             this.ResumeLayout(false);
 
         }
