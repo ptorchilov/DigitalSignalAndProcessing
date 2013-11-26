e805b698a5812bcfc4ed1898140142434ffd3966
p.torchilov <p.torchilov@gmail.com>
2013-11-27 01:25:27 +0300

    Add graphs for Walsh transforms.

--- Lab03-Walsh/ApplicationForm.Designer.cs
+++ Lab03-Walsh/ApplicationForm.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.originalFunction = new ZedGraph.ZedGraphControl();
+            this.walshTransform = new ZedGraph.ZedGraphControl();
+            this.reverseFunction = new ZedGraph.ZedGraphControl();
+            this.drawButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // originalFunction
+            // 
+            this.originalFunction.Location = new System.Drawing.Point(12, 12);
+            this.originalFunction.Name = "originalFunction";
+            this.originalFunction.ScrollGrace = 0D;
+            this.originalFunction.ScrollMaxX = 0D;
+            this.originalFunction.ScrollMaxY = 0D;
+            this.originalFunction.ScrollMaxY2 = 0D;
+            this.originalFunction.ScrollMinX = 0D;
+            this.originalFunction.ScrollMinY = 0D;
+            this.originalFunction.ScrollMinY2 = 0D;
+            this.originalFunction.Size = new System.Drawing.Size(323, 332);
+            this.originalFunction.TabIndex = 0;
+            // 
+            // walshTransform
+            // 
+            this.walshTransform.Location = new System.Drawing.Point(341, 12);
+            this.walshTransform.Name = "walshTransform";
+            this.walshTransform.ScrollGrace = 0D;
+            this.walshTransform.ScrollMaxX = 0D;
+            this.walshTransform.ScrollMaxY = 0D;
+            this.walshTransform.ScrollMaxY2 = 0D;
+            this.walshTransform.ScrollMinX = 0D;
+            this.walshTransform.ScrollMinY = 0D;
+            this.walshTransform.ScrollMinY2 = 0D;
+            this.walshTransform.Size = new System.Drawing.Size(325, 332);
+            this.walshTransform.TabIndex = 1;
+            // 
+            // reverseFunction
+            // 
+            this.reverseFunction.Location = new System.Drawing.Point(672, 12);
+            this.reverseFunction.Name = "reverseFunction";
+            this.reverseFunction.ScrollGrace = 0D;
+            this.reverseFunction.ScrollMaxX = 0D;
+            this.reverseFunction.ScrollMaxY = 0D;
+            this.reverseFunction.ScrollMaxY2 = 0D;
+            this.reverseFunction.ScrollMinX = 0D;
+            this.reverseFunction.ScrollMinY = 0D;
+            this.reverseFunction.ScrollMinY2 = 0D;
+            this.reverseFunction.Size = new System.Drawing.Size(325, 332);
+            this.reverseFunction.TabIndex = 2;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(804, 395);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(149, 40);
+            this.drawButton.TabIndex = 3;
+            this.drawButton.Text = "Draw Graphs";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.DrawButtonClick);
             // 
             // ApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1009, 474);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.reverseFunction);
+            this.Controls.Add(this.walshTransform);
+            this.Controls.Add(this.originalFunction);
             this.Name = "ApplicationForm";
             this.Text = "Lab03 - Walsh Transform - v1";
             this.ResumeLayout(false);
@@ -42,6 +103,11 @@
         }
 
         #endregion
+
+        private ZedGraph.ZedGraphControl originalFunction;
+        private ZedGraph.ZedGraphControl walshTransform;
+        private ZedGraph.ZedGraphControl reverseFunction;
+        private System.Windows.Forms.Button drawButton;
     }
 }
 
--- Lab03-Walsh/ApplicationForm.cs
+++ Lab03-Walsh/ApplicationForm.cs
@@ -1,12 +1,67 @@
 ﻿namespace Lab03_Walsh
 {
+    using System;
+    using System.Drawing;
     using System.Windows.Forms;
+    using Lab01_FFTandDFT;
+    using ZedGraph;
 
     public partial class ApplicationForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationForm"/> class.
+        /// </summary>
         public ApplicationForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draws the values.
+        /// </summary>
+        /// <param name="functionVector">The function vector.</param>
+        /// <param name="zedGraphControl">The zed graph control.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="color">The color.</param>
+        private void DrawValues(double[] functionVector, ZedGraphControl zedGraphControl, String title, Color color)
+        {
+            var graphPane = zedGraphControl.GraphPane;
+
+            graphPane.XAxis.Title.Text = "Count number";
+            graphPane.YAxis.Title.Text = "Value of the function";
+            graphPane.Title.Text = title;
+
+            graphPane.CurveList.Clear();
+
+            var pointList = new PointPairList();
+
+            for (var i = 0; i < functionVector.Length; i++)
+            {
+                pointList.Add(i, functionVector[i]);
+            }
+
+            graphPane.AddCurve(String.Empty, pointList, color);
+
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+        }
+
+        /// <summary>
+        /// Draws the button click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void DrawButtonClick(object sender, EventArgs e)
+        {
+            var functionVector = WalshTransformUtils.GetFunctionVector();
+
+            this.DrawValues(functionVector, originalFunction, "y = cos(x) + sin(2x)", Color.Blue);
+
+            functionVector = WalshTransformUtils.GetFastWalshTransform(functionVector, TransformDirection.Direct);
+            this.DrawValues(functionVector, walshTransform, "Walsh Transform", Color.Blue);
+
+            functionVector = WalshTransformUtils.GetFastWalshTransform(functionVector, TransformDirection.Reverse);
+            this.DrawValues(functionVector, reverseFunction, "Reverse Walsh Transform", Color.Blue);
         }
     }
 }
